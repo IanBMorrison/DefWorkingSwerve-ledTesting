402baa95d26ed5ff94c025e31922d0239f9bcb55
GameMakerJ <146382339+SrMrDoodleB@users.noreply.github.com>
2026-01-06 16:36:45 -0500

    Enhance limelight pose handling with null check
    Added null check for limelightMeasurement and optimized the measurement addition logic.

--- src/main/java/frc/robot/subsystems/swervedrive/limelight_pose.java
+++ src/main/java/frc/robot/subsystems/swervedrive/limelight_pose.java
@@ -10,20 +10,33 @@ def estamater(int tagcout, double accuracy){
   LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("");
 
 // Add it to your pose estimator
+  if (limelightMeasurement == null){
+    return;
+  }
   double ambiguity = limelightMeasurement.avgTagAmbiguity;
   double xyStdDev = 0.5 + ambiguity * 2.0;
   double thetaStdDev = Math.toRadians(5 + ambiguity * 20);
   if (limelightMeasurement != null && limelightMeasurement.tagCount > 0 && limelightMeasurement.avgTagAmbiguity < 0.2){
-  
-      m_poseEstimator.setVisionMeasurementStdDevs(
-          VecBuilder.fill(xyStdDev, xyStdDev, thetaStdDev)
-      );
-      m_poseEstimator.addVisionMeasurement(
-          limelightMeasurement.pose,
-          limelightMeasurement.timestampSeconds
-      );
-      Pose2d pose = m_poseEstimator.getEstimatedPosition();
+      if (limelightMeasurement.tagCount > 0){
+        m_poseEstimator.setVisionMeasurementStdDevs(
+            VecBuilder.fill(xyStdDev, xyStdDev, thetaStdDev)
+        );
+        m_poseEstimator.addVisionMeasurement(
+            limelightMeasurement.pose,
+            limelightMeasurement.timestampSeconds
+        );
+        // put Led change
+      }
 
+      if (limelightMeasurement.tagCount > 0){
+          m_poseEstimator.setVisionMeasurementStdDevs(
+              VecBuilder.fill(xyStdDev, xyStdDev, thetaStdDev)
+          );
+          m_poseEstimator.addVisionMeasurement(
+              limelightMeasurement.pose,
+              limelightMeasurement.timestampSeconds
+          );
+        // put Led change
+      }
   }
 }
-estamate(limelightMeasurement.tagCount, limelightMeasurement.avgTagAmbiguity);
