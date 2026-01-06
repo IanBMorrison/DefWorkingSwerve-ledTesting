8993d8098335fd930cb00ea56d77d71001014fc3
GameMakerJ <146382339+SrMrDoodleB@users.noreply.github.com>
2026-01-06 16:21:46 -0500

    Clean up limelight_pose.java method estamater
    Removed unnecessary variable declarations and comments for clarity.

--- src/main/java/frc/robot/subsystems/swervedrive/limelight_pose.java
+++ src/main/java/frc/robot/subsystems/swervedrive/limelight_pose.java
@@ -1,14 +1,6 @@
 
 def estamater(int tagcout, double accuracy){
-  
-  double tx = LimelightHelpers.getTX("");  // Horizontal offset from crosshair to target in degrees
-  double ty = LimelightHelpers.getTY("");  // Vertical offset from crosshair to target in degrees
-  double ta = LimelightHelpers.getTA("");  // Target area (0% to 100% of image)
-  boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
 
-  double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
-  double tync = LimelightHelpers.getTYNC("");  // Vertical  offset from principal pixel/point to target in degrees
-  // First, tell Limelight your robot's current orientation
   LimelightHelpers.setPipelineIndex("", 9);
 
   double robotYaw = -m_gyro.getYaw();  
