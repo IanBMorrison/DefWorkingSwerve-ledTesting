e0a4f0cf284887d40bfb65b4442de0a87510c7fb
GameMakerJ <146382339+SrMrDoodleB@users.noreply.github.com>
2026-01-07 16:28:55 -0500

    Rename and override method for vision updates

--- src/main/java/frc/robot/subsystems/swervedrive/test_limelight.java
+++ src/main/java/frc/robot/subsystems/swervedrive/test_limelight.java
@@ -17,8 +17,8 @@ public class LimelightSubsystem extends SubsystemBase {
         m_gyro = gyro;
     }
 
-    
-    public void updat_vision() {
+    @Override
+    public void periodic() {
         updateVisionEstimate();
     }
 
