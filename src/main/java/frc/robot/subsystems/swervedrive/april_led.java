207e094b372ef864ee52a4ee3987fb8c336ccbfc
KabirSindhwani <sindhwani.kabir@gmail.com>
2026-01-08 15:29:40 -0500

    Implement Limelight LED control based on AprilTag detection

--- src/main/java/frc/robot/subsystems/swervedrive/april_led.java
+++ src/main/java/frc/robot/subsystems/swervedrive/april_led.java
@@ -1,1 +1,26 @@
+package frc.robot.subsystems.swervedrive;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+
+public class Limelight_LED_Test extends SubsystemBase {
+
+    @Override
+    public void periodic() {
+        LimelightHelpers.setPipelineIndex("", 9);
+        LimelightHelpers.LimelightResults results =
+                LimelightHelpers.getLatestResults("");
+                
+
+        boolean seesAprilTag =
+                results != null &&
+                results.targets_Fiducials != null &&
+                results.targets_Fiducials.length > 0;
+
+        if (seesAprilTag) {
+            LimelightHelpers.setLEDMode_ForceBlink("");
+        } else {
+            LimelightHelpers.setLEDMode_ForceOff("");
+        }
+    }
+}
